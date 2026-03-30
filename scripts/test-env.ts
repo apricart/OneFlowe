f57f6effa7a8ac5c1a21490c075fcdca395722f3
RedzoEbad <ebadkhan6292002@gmail.com>
2026-03-30 11:58:38 +0500

    cleaned version

--- scripts/test-env.ts
+++
@@ -1,6 +0,0 @@
-const dotenv = require("dotenv")
-const result = dotenv.config({ path: ".env.local" })
-console.log("Dotenv result:", result)
-console.log("DATABASE_URL:", process.env.DATABASE_URL)
-
-export { }
